a87d6c611cf004accfe75b0685a9be0798710845
Audrey Tabler <101310073+audreytabler@users.noreply.github.com>
2024-01-26 12:22:18 -0600

    Phaser setup, game object layout, responsive game canvas size based off browser window

---
+++ main.js
@@ -0,0 +1,96 @@
+import './style.css'
+import Phaser from 'phaser'
+
+const sizes = {
+    width: 1920,
+    height: 1080
+}
+
+class GameScene extends Phaser.Scene {
+    //this constructor is basically side bar with all gameobjects in scene
+    constructor() {
+        super("scene-game");
+        //gameObjects
+        this.cameras
+        this.player
+        this.cursor
+
+        //variables
+        this.playerSpeed = 150
+
+        //brainstorming idea: choose character type at beginning of game
+        //ie gym type uses less energy at gym, academic studying decreases stress but social raises it more
+        
+        //keep grade by going to class
+        //doing good on test minigame increases grade
+        this.grade
+
+        //raise by eating & sleeping, decreases through day, low energy = slower movespeed
+        //use energy to go to gym, gym reduces stress & increases energy for next day
+        this.energy
+        
+        //increases via studying, social drama (random event), and not cleaning room
+        //decreases via cleaning, good social (social is a minigame?), & meditating,
+        this.stress
+
+        //if social levels get too low, lonely energy debuff 
+        //high energy levels = study group = studying doesn't increase stress
+        this.social
+
+
+        //soundfx
+        this.bgMusic
+
+    }
+
+    preload() {
+        this.load.image("bg","/assets/bg.png")
+        this.load.image("player","/assets/player.png")
+    }
+
+    create() {
+        this.add.image(0,0,"bg").setOrigin(0,0)
+
+        this.cursor = this.input.keyboard.createCursorKeys()
+
+        //player stuff
+        this.player = this.physics.add.image((window.innerWidth/2),(window.innerHeight/2),"player").setOrigin(0,0)
+        this.player.setImmovable(true)
+        this.player.body.allowGravity=false;
+        this.player.setCollideWorldBounds(true)
+        this.player.setSize(80,15).setOffset(10,70)
+
+    }
+
+    update() {
+
+    }
+}
+
+window.onload = function () {
+    const config = {
+        type: Phaser.WEBGL,
+        canvas: gameCanvas,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 50 },
+                debug: true,
+            },
+        },
+        scene: [GameScene],
+    };
+
+    const game = new Phaser.Game(config);
+
+    //set initial game size
+    game.canvas.width = window.innerWidth - 10;
+    game.canvas.height = window.innerHeight - 10;
+
+    //game canvas resize when user resizes browser window
+    window.addEventListener('resize', function () {
+        //console.log("Window resized! innerWidth is " + window.innerWidth + " window height is " + window.innerHeight)
+        game.canvas.width = window.innerWidth - 10;
+        game.canvas.height = window.innerHeight - 10
+    });
+};
